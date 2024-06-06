1c08a6853e22a3081c807c842135db9715ffd287
igor <igorabeloaz@gmail.com>
2024-06-06 17:01:44 -0300

    subindo arquivos

---
+++ avaliacao.py
@@ -0,0 +1,17 @@
+class Avaliação:
+    def __init__(self,cliente,nota):
+        self._cliente=cliente
+        self._nota=nota
+
+
+
+
+
+    # alternar um estado de um restaurante
+    Restaurante_japones.alternar_estado()
+
+
+
+
+
+    
